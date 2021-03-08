aa667a724bd94002a51071fea2fa4c90bbc0217a
habayeb2 <80113408+habayeb2@users.noreply.github.com>
2021-03-09 03:05:25 +0400

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+number = int(input("Enter any number: "))
+
+if (number % 2) == 0:
+    print(number,"is an even number")
+else:
+    print(number,"is an odd number")

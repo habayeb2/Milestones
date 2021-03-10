328c1bef2f011920a6f884748d2dbbb3b74e305d
JUANREY618 <80118561+JUANREY618@users.noreply.github.com>
2021-03-09 18:01:27 -0600

    Update Milestone1.py

--- Milestone1.py
+++ Milestone1.py
@@ -126,3 +126,31 @@ def mendels_law(hom, het, rec):
     dominant += het*rec*0.5
     prob = dominant/outcomes
     return prob
+
+genetic_code = {
+        'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+        'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+
+        'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+        'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+
+        'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+
+        'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+        'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    }
+def source_rna(protein):
+    rna_combos = 1      #acts as a starting point for the math when finding combos
+    rna_counter = []    #houses the number of occurences of each protein based on its key
+    for n in range (0,len(protein)):        #for every individual protein in the string
+        counter = 0     #the number of occurences of each protein in the dictionary
+        for key in genetic_code:        #goes through every single triplet combination
+            if genetic_code[key] != protein[n:n+1]:     #if the dictionary output isn't the protein we want, ignore it
+                continue
+            else:       #if it is, add one to the number of occurences
+                counter += 1
+        rna_counter.append(counter)     #adds the counted occurences to the list
+    for x in rna_counter:       #takes all the stored number of occurences and multiplies them together
+            rna_combos = rna_combos*x
+    return rna_combos

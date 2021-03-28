6602022e4b9f205ef914f12e572baa09d013ce9e
habayeb2 <80113408+habayeb2@users.noreply.github.com>
2021-03-29 03:40:21 +0400

    Update milestone2.py

--- milestone2.py
+++ milestone2.py
@@ -1,2 +1,12 @@
 
 # milestone 2
+
+def find_motif(dna_motif, dna):
+    index = 0 
+    indexarray = [] 
+    for i in range(0, len(dna_motif)):
+        index = dna.find(dna_motif[i], index)
+        indexarray.append(index) 
+        index += 1 
+    
+    return indexarray

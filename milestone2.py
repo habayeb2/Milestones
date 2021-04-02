494f4d10a26054531e9394e9776895770bea52d5
JUANREY618 <80118561+JUANREY618@users.noreply.github.com>
2021-04-02 12:10:44 -0500

    Update milestone2.py

--- milestone2.py
+++ milestone2.py
@@ -1,7 +1,8 @@
 
 # milestone 2
+import math
 
-def find_motif(dna_motif, dna):
+def find_splice(dna_motif, dna):
     index = 0 
     indexarray = [] 
     for i in range(0, len(dna_motif)):
@@ -10,3 +11,24 @@ def find_motif(dna_motif, dna):
         index += 1 
     
     return indexarray
+
+def perfect_match(rna):
+    rna = rna.upper()
+    matches = 0
+    if rna.count("A") == rna.count("U") and rna.count("C") == rna.count("G"):
+        matches = math.factorial(rna.count("A"))*math.factorial(rna.count("C"))
+    else:
+        matches = matches
+    return matches
+
+def random_genome(dna,gc_content):
+    dna = dna.upper()
+    prob = []
+    for i in range(len(gc_content)):
+        perc = 1
+        perc = perc*((1-gc_content[i])/2)**(dna.count("A"))
+        perc = perc*((1-gc_content[i])/2)**(dna.count("T"))
+        perc = perc*(gc_content[i]/2)**(dna.count("G"))
+        perc = perc*(gc_content[i]/2)**(dna.count("C"))
+        prob.append(math.log10(perc))
+    return prob

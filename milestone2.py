5c7eb848b689b879add68f386a9a318516638126
habayeb2 <80113408+habayeb2@users.noreply.github.com>
2021-04-03 02:47:23 +0400

    Update milestone2.py

--- milestone2.py
+++ milestone2.py
@@ -1,14 +1,15 @@
 
 # milestone 2
 
+import math
+
 def find_splice(dna_motif, dna):
-    index = 0 
-    indexarray = [] 
+    index = 0
+    indexarray = []
     for i in range(0, len(dna_motif)):
         index = dna.find(dna_motif[i], index)
-        indexarray.append(index) 
-        index += 1 
-    
+        indexarray.append(index + 1)
+        index += 2
     return indexarray
 
 def perfect_match(rna):
@@ -16,20 +17,20 @@ def perfect_match(rna):
     rna = rna.upper()
     matches = 0
     if rna.count("A") == rna.count("U") and rna.count("C") == rna.count("G"):
-        matches = math.factorial(rna.count("A"))*math.factorial(rna.count("C"))
+        matches = math.factorial(rna.count("A")) * math.factorial(rna.count("C"))
     else:
         matches = matches
     return matches
 
-def random_genome(dna,gc_content):
+def random_genome(dna, gc_content):
     import math
     dna = dna.upper()
     prob = []
     for i in range(len(gc_content)):
         perc = 1
-        perc = perc*((1-gc_content[i])/2)**(dna.count("A"))
-        perc = perc*((1-gc_content[i])/2)**(dna.count("T"))
-        perc = perc*(gc_content[i]/2)**(dna.count("G"))
-        perc = perc*(gc_content[i]/2)**(dna.count("C"))
+        perc = perc * ((1 - gc_content[i]) / 2) ** (dna.count("A"))
+        perc = perc * ((1 - gc_content[i]) / 2) ** (dna.count("T"))
+        perc = perc * (gc_content[i] / 2) ** (dna.count("G"))
+        perc = perc * (gc_content[i] / 2) ** (dna.count("C"))
         prob.append(math.log10(perc))
     return prob

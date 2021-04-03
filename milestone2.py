7e8686753ca3aadc948158c26ae0f42c298a2621
habayeb2 <80113408+habayeb2@users.noreply.github.com>
2021-04-03 04:17:01 +0400

    Update milestone2.py

--- milestone2.py
+++ milestone2.py
@@ -36,3 +36,26 @@ def random_genome(dna, gc_content):
         perc = perc * (gc_content[i] / 2) ** (dna.count("C"))
         prob.append(math.log10(perc))
     return prob
+
+def reverse_complement(s):
+    complements = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+    return ''.join([complements[c] for c in reversed(s)])
+
+def reverse_palindromes(s):
+    results = []
+
+    l = len(s)
+
+    for i in range(l):
+        for j in range(4, 9):
+
+            if i + j > l:
+                continue
+
+            s1 = s[i:i+j]
+            s2 = reverse_complement(s1)
+
+            if s1 == s2:
+                results.append((i, j))
+
+    return results
